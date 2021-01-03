f4dad5ce9c86639c1d41e2ac0389a7d39535b3a9
zyderus <zyderus@gmail.com>
2021-01-03 23:11:49 +0500

    components approach

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const signinBtn = document.querySelector('#signin-btn')
 const registerBtn = document.querySelector('#register-btn')
 const logoutBtn = document.querySelector('#logout-btn')
 
+footerDate.innerHTML = `${new Date().getFullYear()}`
+
 // Event Listeners
 document.addEventListener('DOMContentLoaded', displayTimelines())
 signinBtn.addEventListener('click', () => showLogin())
@@ -26,44 +28,58 @@ regForm.addEventListener('submit', (e) => {
   registerUser()
 })
 
-footerDate.innerHTML = `${new Date().getFullYear()}`
-
 // Functions
+function clearSections() {
+  document.querySelectorAll('section').forEach(section => {
+    section.style.display = 'none'
+  })
+}
+
 function showFeed() {
+  clearSections()
   feedSection.style.display = 'block'
-  regSection.style.display = 'none'
-  loginSection.style.display = 'none'
-  pageSection.style.display = 'none'
 }
 
 function showLogin() {
-  feedSection.style.display = 'none'
-  regSection.style.display = 'none'
+  clearSections()
   loginSection.style.display = 'block'
-  pageSection.style.display = 'none'
 }
 
 function showRegister() {
-  feedSection.style.display = 'none'
+  clearSections()
   regSection.style.display = 'block'
-  loginSection.style.display = 'none'
-  pageSection.style.display = 'none'
 }
 
 function showPage() {
+  clearSections()
   signinBtn.style.display = 'none'
   registerBtn.style.display = 'none'
-  feedSection.style.display = 'none'
-  regSection.style.display = 'none'
-  loginSection.style.display = 'none'
   pageSection.style.display = 'block'
   logoutBtn.style.display = 'block'
 }
 
+function timeFormat(ms) {
+  const now = Date.now()
+  let date
+  if (now - ms < 60 * 1000) {
+    date = 'now'
+  } else if (now - ms < 10 * 60 * 1000) {
+    date = 'within 10 mins'
+  } else if (now - ms < 60 * 60 * 1000) {
+    date = 'an hour ago'
+  } else if (now - ms < 12 * 60 * 60 * 1000) {
+    date = 'today'
+  } else {
+    date = new Date(ms).toDateString()
+  }
+  return date
+}
+
 function displayTimelines() {
   showFeed()
+  newsfeed.sort((a, b) => b.date - a.date)
   newsfeed.forEach(feed => {
-    createCard(feed, feedSection)
+    feedSection.appendChild(blogCardComponent(feed))
   })
 }
 
@@ -74,21 +90,51 @@ function loginUser() {
   if (foundUser && password.value === foundUser.password) {
     showPage();
     pageSection.innerHTML = `
-      <h2>Welcome to your Dashboard, ${foundUser.username.toUpperCase()}</h2>
-      <br>
-      <div class="card">
+      <div class="heading">
+        <h2>Welcome to your Dashboard, ${foundUser.username.toUpperCase()}</h2>
+      </div>
+      <div class="card blog-form">
         <form action="" id="add-blog">
           <input type="text" id="img-url" placeholder="image url..."><br>
           <textarea name="" id="blog-text" cols="30" rows="10" placeholder="your blog..."></textarea><br>
-          <button class="button">Submit</button>
+          <a href="#" id="submit-blog" class="button btn-submit">Submit</a>
+          <span class="warning submit-msg"></span>
         </form>
       </div>
     `
-    const list = document.querySelector('#list')
+    const blogForm = document.querySelector('.blog-form')
+    const submitBtn = document.querySelector('#submit-blog')
+    const imgUrl = document.querySelector('#img-url')
+    const blogText = document.querySelector('#blog-text')
+    const submitMsg = document.querySelector('.submit-msg')
+
+    newsfeed.sort((a, b) => b.date - a.date)
     newsfeed.forEach(feed => {
-      if (feed.username === foundUser.username) {
-        createCard(feed, pageSection)
+      if (feed.username === foundUser.username){
+        pageSection.appendChild(blogCardComponent(feed))
       }
+    })
+
+    submitBtn.addEventListener('click', () => {
+      if (!(blogText.value.length > 1)) {
+        return submitMsg.innerHTML = ' * Cannot submit an empty blog...'
+      }
+      
+      submitMsg.innerHTML = ''
+      const newBlog = {
+        username: foundUser.username,
+        date: Date.now(),
+        img: imgUrl.value,
+        timeline: blogText.value,
+      }
+      newsfeed.push(newBlog)
+      writeToLocalstorage('newsfeed', newsfeed)
+
+      imgUrl.value = ''
+      blogText.value = ''
+
+      // Insert a new blog
+      blogForm.after(blogCardComponent(newBlog))
     })
 
   } else {
@@ -112,37 +158,54 @@ function registerUser() {
 
       showPage();
       pageSection.innerHTML = `
+      <div class="heading">
         <h2>Welcome to your Dashboard, ${username.value.toUpperCase()}</h2>
-        <a href="index.html"><button>Back</button></a>
+      </div>
       `
     } else {
-      showPage()
-      pageSection.innerHTML = `
-        <h4>Username and Password must be 3 or more characters
+      showRegister()
+      regSection.innerHTML = ``
+      regSection.innerHTML = `
+      <div class="card">
+        <p class="warning">* Username and Password must be 3 or more characters</p>
+      </div>
       `
-      setTimeout(() => { window.location.href = "index.html"; }, 2000)
+      setTimeout(() => { window.location.href = "index.html"; }, 3000)
     }
   } else {
     showPage();
     pageSection.innerHTML = `
-      <h2>Username already registered. Please proceed to Login page</h2>
+    <div class="card">
+      <p class="warning">* Username already registered. Please proceed to Login page</p>
+    </div>
     `
-    setTimeout(() => { window.location.href = "index.html"; }, 2000)
+    setTimeout(() => { window.location.href = "index.html"; }, 3000)
   }
 }
 
-function createCard(feed, section) {
+// function deleteBlog() {
+//   console.log('blog deleted')
+// }
+
+// Components
+function blogCardComponent(feed) {
   const card = document.createElement('div')
   card.className = 'card'
   card.innerHTML = `
+    <div class="delete-btn">&Cross;</div>
     <div class="pic-container" style="
       background-image: url('${feed.img}');">
     </div>
     <div class="content">
       <h3>${feed.username}</h3>
-      <span class="date-span">${feed.date}</span>
+      <span class="date-span">${timeFormat(feed.date)}</span>
       <p>${feed.timeline.substring(0, 500)}</p>
     </div>
   `
-  section.appendChild(card)
+  const deleteBtn = card.querySelector('.delete-btn')
+  deleteBtn.addEventListener('click', () => {
+    card.style.transform = 'translateX(-3rem)'
+    // card.remove()
+  })
+  return card
 }
