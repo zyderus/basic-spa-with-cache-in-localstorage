b3589dee96fd175057c160bbbb9b26f18462d816
zyderus <zyderus@gmail.com>
2021-01-04 06:09:06 +0500

    add subtle parallax

--- script.js
+++ script.js
@@ -1,61 +1,53 @@
-const feedSection = document.querySelector('#feed')
-const regSection = document.querySelector('#register')
-const loginSection = document.querySelector('#login')
-const pageSection = document.querySelector('#page')
-const regForm = document.querySelector('#reg-form')
-const loginForm = document.querySelector('#login-form')
-const toLogin = document.querySelector('#to-login')
-const toRegister = document.querySelector('#to-register')
-const footerDate = document.querySelector('.footer-date')
+const body = document.querySelector('body')
+const main = document.querySelector('#main')
 const signinBtn = document.querySelector('#signin-btn')
 const registerBtn = document.querySelector('#register-btn')
 const logoutBtn = document.querySelector('#logout-btn')
 
-footerDate.innerHTML = `${new Date().getFullYear()}`
+// Add footer
+const footer = footerComponent()
+body.appendChild(footer)
 
 // Event Listeners
-document.addEventListener('DOMContentLoaded', displayTimelines())
-signinBtn.addEventListener('click', () => showLogin())
-registerBtn.addEventListener('click', () => showRegister())
-
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  loginUser()
+document.addEventListener('DOMContentLoaded', displayTimeline())
+window.addEventListener('scroll', () => {
+  const offset = window.pageYOffset
+  body.style.backgroundPositionY = offset * 0.2 + 'px'
 })
-
-regForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  registerUser()
-})
+signinBtn.addEventListener('click', () => loginUser())
+registerBtn.addEventListener('click', () => registerUser())
 
 // Functions
-function clearSections() {
-  document.querySelectorAll('section').forEach(section => {
-    section.style.display = 'none'
+function signedBtnSet() {
+  signinBtn.style.display = 'none'
+  registerBtn.style.display = 'none'
+  logoutBtn.style.display = 'block'
+}
+
+function displayTimeline() {
+  newsfeed.sort((a, b) => b.date - a.date)
+  newsfeed.forEach(feed => {
+    main.appendChild(blogCardComponent(feed))
   })
 }
 
-function showFeed() {
-  clearSections()
-  feedSection.style.display = 'block'
+function loginUser() {
+  main.innerHTML = ''
+  const card = loginCardComponent()
+  main.appendChild(card)
 }
 
-function showLogin() {
-  clearSections()
-  loginSection.style.display = 'block'
+function registerUser() {
+  main.innerHTML = ''
+  const card = registerCardComponent()
+  main.appendChild(card)
 }
 
-function showRegister() {
-  clearSections()
-  regSection.style.display = 'block'
-}
-
-function showPage() {
-  clearSections()
-  signinBtn.style.display = 'none'
-  registerBtn.style.display = 'none'
-  pageSection.style.display = 'block'
-  logoutBtn.style.display = 'block'
+// Remove a blog from local storage
+function removeBlogFromDb(element) {
+  const index = newsfeed.findIndex(feed => feed.date === element)
+  newsfeed.splice(index, 1)
+  writeToLocalstorage('newsfeed', newsfeed)
 }
 
 function timeFormat(ms) {
@@ -73,86 +65,6 @@ function timeFormat(ms) {
     date = new Date(ms).toDateString()
   }
   return date
-}
-
-function displayTimelines() {
-  showFeed()
-  newsfeed.sort((a, b) => b.date - a.date)
-  newsfeed.forEach(feed => {
-    feedSection.appendChild(blogCardComponent(feed))
-  })
-}
-
-function loginUser() {
-  const username = document.querySelector('#login-username')
-  const password = document.querySelector('#login-password')
-  const foundUser = users.find(user => username.value === user.username)
-  if (foundUser && password.value === foundUser.password) {
-    showPage();
-    insertBlogComponent(foundUser)
-    newsfeed.sort((a, b) => b.date - a.date)    // sort blogs by date in descending order
-    // Add blog cards of a login user
-    newsfeed.forEach(feed => {
-      if (feed.username === foundUser.username){
-        const card = blogCardComponent(feed)
-        card.querySelector('.delete-btn').style.display = 'flex'
-        pageSection.appendChild(card)
-      }
-    })
-  } else {
-    username.value = ''
-    password.value = ''
-  }
-}
-
-function registerUser() {
-  const username = document.querySelector('#reg-username')
-  const password = document.querySelector('#reg-password')
-
-  // Check if username exists
-  const foundUser = users.find(user => username.value === user.username)
-  if (!foundUser) {
-    // if username and password contain at least 3 chars, then create a user
-    if (username.value.length > 2 && password.value.length > 2) {
-      const newUser = {
-        username: username.value,
-        password: password.value,
-      }
-      // Add user to array to local storage
-      users.push(newUser)
-      writeToLocalstorage('users', users)
-
-      // unhide page section
-      showPage();
-      insertBlogComponent(newUser)
-    } else {
-      showRegister()
-      regSection.innerHTML = ``
-      regSection.innerHTML = `
-      <div class="card">
-        <p class="warning">* Username and Password must be 3 or more characters</p>
-      </div>
-      `
-      // Back to main screen after n milliseconds
-      setTimeout(() => { window.location.href = "index.html"; }, 3000)
-    }
-  } else {
-    showPage();
-    pageSection.innerHTML = `
-    <div class="card">
-      <p class="warning">* Username already registered. Please proceed to Login page</p>
-    </div>
-    `
-    setTimeout(() => { window.location.href = "index.html"; }, 3000)
-  }
-}
-
-// Remove a blog from local storage
-function removeBlogFromDb(element) {
-  const blogs = readFromLocalstorage('newsfeed')
-  const index = blogs.findIndex(blog => blog.date === element)
-  blogs.splice(index, 1)
-  writeToLocalstorage('newsfeed', blogs)
 }
 
 /*   COMPONENTS   */
@@ -188,7 +100,7 @@ function blogCardComponent(feed) {
 
 // Insert and display a blog component
 function insertBlogComponent(user) {
-  pageSection.innerHTML = `
+  main.innerHTML = `
     <div class="heading">
       <h2>Welcome to your Dashboard, ${user.username.toUpperCase()}</h2>
     </div>
@@ -237,4 +149,124 @@ function insertBlogComponent(user) {
     card.querySelector('.delete-btn').style.display = 'flex'
     blogForm.after(card)
   })
+}
+
+// Login card component
+function loginCardComponent() {
+  const card = document.createElement('div')
+  card.className = 'card'
+  card.className += ' card-auth'
+  card.innerHTML = `
+    <div class="auth-title">Sign In</div>
+
+    <form action="" id="login-form">
+      <label for="login-username">Username</label><br>
+      <input type="text" id="login-username"><br>
+      <label for="login-password">Password</label><br>
+      <input type="password" id="login-password"><br>
+      <button class="button btn-glass">Login</button>
+    </form> 
+  `
+  const username = card.querySelector('#login-username')
+  const password = card.querySelector('#login-password')
+  const loginForm = card.querySelector('#login-form')
+
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    login()
+  })
+
+  function login() {
+    const foundUser = users.find(user => username.value === user.username)
+    if (foundUser && password.value === foundUser.password) {
+      signedBtnSet()
+      insertBlogComponent(foundUser)
+      newsfeed.sort((a, b) => b.date - a.date)    // sort blogs by date in descending order
+      // Add blog cards of a login user
+      newsfeed.forEach(feed => {
+        if (feed.username === foundUser.username){
+          const card = blogCardComponent(feed)
+          card.querySelector('.delete-btn').style.display = 'flex'
+          main.appendChild(card)
+        }
+      })
+    } else {
+      username.value = ''
+      password.value = ''
+    }
+  }
+  return card
+}
+
+// Register card component
+function registerCardComponent() {
+  const card = document.createElement('div')
+  card.className = 'card'
+  card.className += ' card-auth'
+  card.innerHTML = `
+    <div class="auth-title">Register</div>
+
+    <form action="" id="reg-form">
+      <label for="reg-username">Username</label><br>
+      <input type="text" id="reg-username"><br>
+      <label for="reg-password">Password</label><br>
+      <input type="password" id="reg-password"><br>
+      <button class="button btn-glass">Register</button>
+    </form>
+  `
+  const username = card.querySelector('#reg-username')
+  const password = card.querySelector('#reg-password')
+  const regForm = card.querySelector('#reg-form')
+
+  regForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    register()
+  })
+
+  function register() {
+    const foundUser = users.find(user => username.value === user.username)
+    if (!foundUser) {
+      // if username and password contain at least 3 chars, then create a user
+      if (username.value.length > 2 && password.value.length > 2) {
+        signedBtnSet()
+        const newUser = {
+          username: username.value,
+          password: password.value,
+        }
+        // Add user to array to local storage
+        users.push(newUser)
+        writeToLocalstorage('users', users)
+  
+        insertBlogComponent(newUser)
+      } else {
+        main.innerHTML = ``
+        main.innerHTML = `
+        <div class="card">
+          <p class="warning">* Username and Password must be 3 or more characters</p>
+        </div>
+        `
+        // Back to main screen after n milliseconds
+        setTimeout(() => { window.location.href = "index.html"; }, 3000)
+      }
+    } else {
+      main.innerHTML = `
+      <div class="card">
+        <p class="warning">* Username already registered. Please proceed to Login page</p>
+      </div>
+      `
+      setTimeout(() => { window.location.href = "index.html"; }, 3000)
+    }
+  }
+  return card
+}
+
+function footerComponent() {
+  const footer = document.createElement('footer')
+  footer.className = 'footer'
+  footer.innerHTML = `
+    <br><br><hr><br>
+    &copy; ${new Date().getFullYear()} MugMag, Inc.
+    <br><br><br>
+  `
+  return footer
 }
