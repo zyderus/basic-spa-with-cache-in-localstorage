2889fca55f131aa93eb6698b852b2a5371946c1b
zyderus <zyderus@gmail.com>
2021-07-09 01:53:00 +0500

    fix script module

--- server.ts
+++ server.ts
@@ -4,14 +4,10 @@ import path from 'path'
 const server = express()
 const port: string | number = process.env.PORT || 5000
 
-// server.use(express.static(path.join(__dirname, '/public')))
+server.use(express.static(path.join(__dirname, '/public')))
 
 server.get('/', (req: Request, res: Response) => {
-  res.send('<h1>Testing Ninja || Awesome result</h1>')
+  res.sendFile(path.resolve(__dirname, './public/index.html'))
 })
-
-// server.get('/', (req: Request, res: Response) => {
-//   res.sendFile(path.resolve(__dirname, './public/index.html'))
-// })
 
 server.listen(port, () => console.log(`server is running @ http://localhost:${port}`))
